1e3566fc293f839e0fb980964b5734a10fe60873
Manuel Silva <manuel@mjs.me>
2022-12-19 16:36:56 +0000

    Add ability to add books and basic JS storage and structure

--- main.js
+++ main.js
@@ -1,6 +1,81 @@
 let app;
 
+/**
+ *
+ * @param title {string}
+ * @param author {string}
+ * @param pages {number}
+ * @param imageUrl {string}
+ * @constructor
+ */
+function Book(title, author, pages, imageUrl) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.imageUrl = imageUrl;
+}
+
+/**
+ *
+ * @param id {number}
+ * @param book {Book}
+ * @constructor
+ */
+function Record(id, book) {
+  this.id = id;
+  this.book = book;
+}
+
+function Storage() {
+  this.lastId = 0;
+  // this.db = [];
+  this.db = [
+    new Record(
+      1,
+      new Book(
+        "Fifty Shades of Gray",
+        "E. L. James",
+        514,
+        "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTRl34v2iRF1MQfzwDOuop_3akLq9EFutHbhXAra9Gs_UKK8M0o"
+      )
+    ),
+    new Record(
+      2,
+      new Book(
+        "Harry Potter and the Philosopher's Stone",
+        "J. K. Rowling",
+        812,
+        "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRkWXbfQrO5oTm1I5izjeRTbQD-vKPnybT24S2kFw2eH8LyyCo5"
+      )
+    ),
+  ];
+
+  /**
+   * Add new book to storage
+   *
+   * @param book {Book}
+   */
+  this.add = function (book) {
+    const id = this.lastId;
+    this.db.push(new Record(id, book));
+    this.lastId = id + 1;
+  };
+
+  /**
+   * Deletes book by id
+   * @param index {number}
+   */
+  this.remove = function (index) {
+    // this.db[index] || delete this.db[index];
+  };
+
+  this.count = function () {
+    return this.db.length;
+  };
+}
+
 function App() {
+  let storage = null;
   const hideAllViews = () => {
     [...document.querySelectorAll(".main section")].forEach((el) => {
       el.classList.remove("visible");
@@ -12,9 +87,66 @@ function App() {
     el.classList.add("visible");
   };
 
-  this.switchView = function (viewName) {
+  /**
+   *
+   * @returns {Storage}
+   */
+  const getStorage = () => {
+    storage = storage || new Storage();
+    return storage;
+  };
+
+  /**
+   *
+   * @param book {Book}
+   */
+  const validateBookDataAndDisplayErrors = (book) => {
+    let hasNoErrors = true;
+    [...Object.getOwnPropertyNames(book)].forEach((elementName) => {
+      document.querySelector(`#${elementName}`).classList.remove("error");
+      document.querySelector(`#${elementName} ~ .error-message`).innerHTML = "";
+
+      if (!book[elementName]) {
+        document.querySelector(`#${elementName}`).classList.add("error");
+        document.querySelector(
+          `#${elementName} ~ .error-message`
+        ).innerHTML = `You need to fill ${elementName}`;
+        hasNoErrors = false;
+      }
+    });
+    return hasNoErrors;
+  };
+
+  const resetBookInputFields = () => {
+    document.querySelectorAll(".input-wrapper input").forEach((element) => {
+      element.value = "";
+    });
+  };
+
+  const updateBookCounter = () => {
+    document.querySelector(".books-counter").innerHTML = getStorage().count();
+  };
+
+  this.switchView = (viewName) => {
     hideAllViews();
     showView(viewName);
+  };
+
+  this.addBook = () => {
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const pages = document.querySelector("#pages").value;
+    const imageUrl = document.querySelector("#imageUrl").value;
+
+    const book = new Book(title, author, pages, imageUrl);
+
+    if (!validateBookDataAndDisplayErrors(book)) {
+      return;
+    }
+
+    getStorage().add(book);
+    resetBookInputFields();
+    updateBookCounter();
   };
 }
 
@@ -22,9 +154,13 @@ document.querySelector("body").style.height = `${window.innerHeight}px`;
 app = app || new App();
 
 // EVENT LISTENERS
-[...document.querySelectorAll(".nav-action-wrapper > span")].forEach((el) => {
+document.querySelectorAll(".nav-action-wrapper > span").forEach((el) => {
   el.addEventListener("click", (event) => {
     event.stopPropagation();
     app.switchView(event.currentTarget.dataset.view);
   });
+});
+
+document.querySelector(".add-book-bto").addEventListener("click", () => {
+  app.addBook();
 });
